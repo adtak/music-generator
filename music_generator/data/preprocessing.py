cdf31669278b8236b241b8e206626d142590853b
adtak <adtak62@gmail.com>
2021-09-17 23:38:39 +0900

    feat: fix preprocessing

--- music_generator/data/preprocessing.py
+++ music_generator/data/preprocessing.py
@@ -3,19 +3,33 @@ from music21 import chord, converter, instrument, note
 from typing import List
 
 
-def get_notes(midi_dir_parh: Path) -> List[str]:
-    all_notes = []
+def load_midi_from_dir(midi_dir_parh: Path) -> List[List[str]]:
+    notes = []
+    durations = []
     for file in midi_dir_parh.glob('*.mid'):
-        notes = []
-        score = instrument.partitionByInstrument(converter.parse(file))
-        for element in score.parts[0].recurse():
-            if isinstance(element, chord.Chord):
-                notes.append('-'.join(str(i) for i in element.normalOrder))
-            elif isinstance(element, note.Note):
-                notes.append(str(element.pitch))
-        all_notes.append(notes)
-        print(f'{file.name} has {len(notes)} notes.')
+        score = instrument.partitionByInstrument(converter.parse(file).chordify())
+        n, d = get_notes_and_durations(score)
+        notes.append(n)
+        durations.append(d)
+    print(f"Load {len(notes)} MIDI files.")
+
+
+def get_notes_and_durations(score) -> List[str]:
+    notes = []
+    durations = []
+    for element in score.flat:
+        if isinstance(element, chord.Chord):
+            notes.append('.'.join(p.nameWithOctave for p in element.pitches))
+            durations.append(element.duration.quarterLength)
+        elif isinstance(element, note.Note):
+            if element.isRest:
+                notes.append(element.name)
+                durations.append(element.duration.quarterLength)
+            else:
+                notes.append(element.nameWithOctave)
+                durations.append(element.duration.quarterLength)
+    return notes, durations
 
 
 if __name__ == '__main__':
-    get_notes(Path('resources'))
+    load_midi_from_dir(Path('resources'))
